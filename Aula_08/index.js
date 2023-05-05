5ba9cba06c8ab3481ec0dacff0a02343aab869ab
Daniel Rosa Pinheiro Junior <daniel.admin@unimed-franca>
2023-05-05 20:24:34 -0300

    Aula pratica , adicionando outros metodos crud na ultima aula do dia 28/04

--- Aula_08/index.js
+++ Aula_08/index.js
@@ -17,22 +17,7 @@ app.get("/getUsers", async (req, res) =>{
         res.status(500).send("Erro de conexão com o servidor");
     }
 })
-/*/app.post("/getUsers", async (req,res) => {
-    try {
-        newUser= {
-            id:  req.body.id,
-            nome: req.body.nome
-        }
-        const insert = await pool.connect();
-        const {rowsInsert} = await insert.query(`INSERT INTO Users (id, nome) values (5,'Tulio'))`); 
-        console.log(rowsInsert);
-        const { rows } = await insert.query ("SELECT * FROM Users");
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Erro na conexao");
-    }
-})/*/
+
 app.post('/getUsers/:name&:id', async (req, res) =>{
     try {
         const { name } = req.params;
@@ -43,6 +28,35 @@ app.post('/getUsers/:name&:id', async (req, res) =>{
         console.table(rows);
         res.status(200).send(rows);
     } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro de conexão com o servidor");
+    }
+});
+
+app.put('/getUsers/:nome&:id', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const { name } = req.params;
+        const { id } = req.params;
+        const { update } = await client.query (`UPDATE Users SET nome='${name}' WHERE id = '${id}'`);
+        const { rows } = await client.query ("SELECT * FROM Users");
+        console.table(rows);
+        res.status(200).send(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro de conexão com o servidor");
+    }
+});
+
+app.delete('/getUsers/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const client = await pool.connect();
+        const { del } = await client.query(`DELETE FROM Users WHERE id = ${id}`);
+        const { rows } = await client.query ('SELECT * FROM Users');
+        console.table(rows);
+        res.status(200).send(rows);
+    }  catch (error) {
         console.error(error);
         res.status(500).send("Erro de conexão com o servidor");
     }
